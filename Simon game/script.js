49d98f758168ada765a610f5780aa0ddc790863a
cheng chen <chencheng1q88@gmail.com>
2018-02-13 21:58:16 +1300

    js for simon

---
+++ Simon game/script.js
@@ -0,0 +1,16 @@
+//simon game for FCC project,
+// Using the OOP paradiam to create this project 
+
+// game Object
+
+var game = {
+	playerTurn: false;
+	sequence: [];
+	sequenceRepeat: false;
+	winSeQuence: ["green","red","blue","yellow"],
+	seqCount : 0;
+	strict: false;
+	audio: {}
+};
+
+
