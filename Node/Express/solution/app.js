59c32524525c41a061aafcedaa6780e4425e361d
cheng chen <chencheng1q88@gmail.com>
2018-03-05 21:03:25 +1300

    Express solution

--- Node/Express/solution/app.js
+++ Node/Express/solution/app.js
@@ -18,6 +18,21 @@ app.get("/speak/: animal", function(req,res){
      res.send("The" + animal + "says" + sounds + "");
 }); 
 
+ app.get("/repeat/:message/:times", function(req, res) {
+     var message = req.params.message;
+     var times = Number(req.params.times);
+     var result = "";
+     
+     for(var i = 0; i < times; i ++){
+         result += message + " ";
+     }
+     res.send(result);
+ });
+ 
+app.get("*", function(req, res) {
+   res.send("sorry, page not found... what are you doing with your life?"); 
+});
+ 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Now serving your app!");
 });
